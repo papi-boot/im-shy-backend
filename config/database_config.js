5f217e0fc1d0d9c5f762eae00771ae5c8d1dc4e5
papi-boot <papi8261@gmail.com>
2022-01-02 16:22:53 +0800

    change database name

--- config/database_config.js
+++ config/database_config.js
@@ -11,11 +11,11 @@ module.exports = {
 
   production: {
     use_env_variable: process.env.DATABASE_URL,
-    username: process.env.DEV_PG_USERNAME,
-    password: process.env.DEV_PG_PASSWORD,
-    database: process.env.DEV_PG_DATABASE,
-    host: process.env.DEV_PG_HOST,
-    port: process.env.DEV_PG_PORT,
+    username: process.env.PROD_PG_USERNAME,
+    password: process.env.PROD_PG_PASSWORD,
+    database: process.env.PROD_PG_DATABASE,
+    host: process.env.PROD_PG_HOST,
+    port: process.env.PROD_PG_PORT,
     dialect: "postgres",
     dialectOptions: {
       ssl: {
